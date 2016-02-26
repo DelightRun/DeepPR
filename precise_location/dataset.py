0ec0e0bef2429544212099ee987f6aa76d28de51
Changxu <Changxu.mail@gmail.com>
2016-02-27 00:13:28 +0800

    归一化做一些修改

--- precise_location/dataset.py
+++ precise_location/dataset.py
@@ -13,13 +13,13 @@ def _load_image(line):
     keypoints = map(int, keypoints.strip().split())
 
     image = cv2.imread(os.path.join(basepath, 'data', filename+'.jpg'))
-    keypoints = numpy.asarray(zip(keypoints[::2], keypoints[1::2]), dtype="int32")
+    keypoints = numpy.asarray(zip(keypoints[::2], keypoints[1::2]), dtype="float32")
 
     width, height = image.shape[1], image.shape[0]
     targetWidth, targetHeight = 240, 120
 
-    keypoints[:,0] = keypoints[:,0] * targetWidth / width
-    keypoints[:,1] = keypoints[:,1] * targetHeight / height
+    keypoints[:,0] = keypoints[:,0] / width
+    keypoints[:,1] = keypoints[:,1] / height
     image = cv2.resize(image, (targetWidth,targetHeight))
     image = image.astype("float32")
     image /= 255.0
--- precise_location/train.py
+++ precise_location/train.py
@@ -7,15 +7,13 @@ from keras.callbacks import ModelCheckpoint, ProgbarLogger
 
 basepath = os.path.dirname(__file__)
 
-print "loading data...",
+print "load data",
 X, y = dataset.load_as_nparray()
 X_train, y_train = X[:800,:,:], y[:800,:]
 X_test, y_test = X[800:,:,:], y[800:,:]
-print "done"
 
-print "compiling model...",
+print "compile model",
 model = models.VGG()
-print "done"
 
 checkpoint = ModelCheckpoint(filepath=os.path.join(basepath, 'checkpoint.hdf5'), verbose=1, save_best_only=True)
 model.fit(X_train, y_train, batch_size=100, nb_epoch=20, verbose=1, validation_data=(X_test, y_test), callbacks=[checkpoint])
