af2c88460e420046cf05e291560873cbe3400d4b
Changxu <Changxu.mail@gmail.com>
2016-06-03 23:20:19 +0800

    答辩完了

--- thesis/auto/DeepPR.el
+++ thesis/auto/DeepPR.el
@@ -3,6 +3,18 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("XDUthesis" "WordOneHalf")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("natbib" "numbers" "sort&compress")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "ThesisFiles/Abstract"
@@ -10,7 +22,8 @@
     "ThesisFiles/Thanks"
     "ThesisFiles/Appendix"
     "XDUthesis"
-    "XDUthesis10")
+    "XDUthesis10"
+    "natbib")
    (LaTeX-add-bibliographies
     "ThesisFiles/RefFile"))
  :latex)
